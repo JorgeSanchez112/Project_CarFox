29b9e76a2ef8a9760f9c6dcf31c31d8d95d35887
Jorge Sanchez <halo3ratons@gmail.com>
2023-01-04 14:49:34 -0500

    DOCS: Maquetado html para las tablas tipo responsive

---
+++ Diseño_V3/neg_dat_consultar_observaciones.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>informe_observaciones</title>
+    <link rel="stylesheet" href="css/Style_consultas.css">
+</head>
+<body>
+    <?php include('header_administrador.php'); ?>
+    <main class="container">
+        <table class="table">
+            <caption>Observaciones</caption>
+            <thead>
+                <tr>
+                    <th>Placa</th>
+                    <th>Descripcion</th>
+                    <th>Cod repuesto</th>
+                    <th>Mecanico</th>
+                    <th>Fecha entrada</th>
+                    <th>Fecha salida</th>
+                    <th>Imagenes</th>
+                    <th>Generar salida</th>
+                    <th>Eliminar</th>
+                    <th>Actualizar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td data-label="Placa">DTE312</td>
+                    <td data-label="Descripcion">Al automovil se le realizo un cambio de bateria con herramientas mecanicas</td>
+                    <td data-label="Cod repuesto">B-300</td>
+                    <td data-label="Mecanico">1067219831</td>
+                    <td data-label="Fecha entrada">2019-03-11 10:29:20</td>
+                    <td data-label="Fecha salida">2019-03-12 10:30:01</td>
+                    <td data-label="Imagenes">
+                        <figure>
+                            <img src="img/small/img_auto2_small.png" alt="auto2">
+                        </figure>
+                    </td>
+                    <td data-label="Generar salida">
+                        <button>Salida</button>
+                    </td>
+                    <td data-label="Eliminar">
+                        <button>Eliminar</button>
+                    </td>
+                    <td data-label="Actualizar">
+                        <button>Actualizar</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Placa">DTE312</td>
+                    <td data-label="Descripcion">Al automovil se le realizo un cambio de bateria con herramientas mecanicas</td>
+                    <td data-label="Cod repuesto">B-300</td>
+                    <td data-label="Mecanico">1067219831</td>
+                    <td data-label="Fecha entrada">2019-03-11 10:29:20</td>
+                    <td data-label="Fecha salida">2019-03-12 10:30:01</td>
+                    <td data-label="Imagenes">
+                        <figure>
+                            <img src="img/small/img_auto1_small.jpg" alt="auto2">
+                        </figure>
+                    </td>
+                    <td data-label="Generar salida">
+                        <button>Salida</button>
+                    </td>
+                    <td data-label="Eliminar">
+                        <button>Eliminar</button>
+                    </td>
+                    <td data-label="Actualizar">
+                        <button>Actualizar</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Placa">DTE312</td>
+                    <td data-label="Descripcion">Al automovil se le realizo un cambio de bateria con herramientas mecanicas</td>
+                    <td data-label="Cod repuesto">B-300</td>
+                    <td data-label="Mecanico">1067219831</td>
+                    <td data-label="Fecha entrada">2019-03-11 10:29:20</td>
+                    <td data-label="Fecha salida">2019-03-12 10:30:01</td>
+                    <td data-label="Imagenes">
+                        <figure>
+                            <img src="img/small/img_auto2_small.png" alt="auto2">
+                        </figure>
+                    </td>
+                    <td data-label="Generar salida">
+                        <button>Salida</button>
+                    </td>
+                    <td data-label="Eliminar">
+                        <button>Eliminar</button>
+                    </td>
+                    <td data-label="Actualizar">
+                        <button>Actualizar</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+</body>
+</html>
