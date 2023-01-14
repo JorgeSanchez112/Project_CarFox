f46d1df44db28b4cc9d6d3d2ca78724c7b9ac34c
Jorge Sanchez <halo3ratons@gmail.com>
2023-01-13 19:27:48 -0500

    Finalizo la implementacion del nuevo diseño responsive

---
+++ Diseño_V3/neg_dat_actualizar_observaciones.php
@@ -0,0 +1,18 @@
+<?php
+
+include ('ventana_observaciones.php');
+
+class Observaciones//inicio clase
+{  
+	public function actualizar($id_observaciones,$placa,$cod_repuesto,$descripcion_reparacion,$documento,$fecha_entrada,$fecha_salida)
+{
+	include("conexion.php");
+
+	mysqli_query($db,"UPDATE observaciones SET fk_placa='$placa', cod_repuesto='$cod_repuesto', descripcion_reparacion='$descripcion_reparacion', documento='$documento', fecha_entrada='$fecha_entrada',fecha_salida='$fecha_salida' WHERE id_observaciones='$id_observaciones'");
+		
+}
+}
+$nuevo =new Observaciones ();
+$nuevo->actualizar($_POST["id_observaciones"],$_POST["placa"],$_POST["cod_repuesto"],$_POST["descripcion_reparacion"],$_POST["documento"],$_POST["fecha_entrada"],$_POST["fecha_salida"]);
+
+?>
--- Diseño_V3/pres_registro_observaciones.php
+++ Diseño_V3/pres_registro_observaciones.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de observaciones</title>
-    <link rel="stylesheet" href="css/Style_registros.css">
+    <link rel="stylesheet" href="css/Style_formularios.css">
 </head>
 <body>
     <?php include('header_administrador.php'); ?>
