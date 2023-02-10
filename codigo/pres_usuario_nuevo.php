b136688fcf58ae2af45e950915735fa7ae81742b
Jorge Sanchez <halo3ratons@gmail.com>
2023-02-10 11:54:13 -0500

    DOCS: Se realizo la pagina de registro del administrador

--- codigo/pres_usuario_nuevo.php
+++ codigo/pres_usuario_nuevo.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Crear usuario</title>
     <link rel="stylesheet" href="css/style_registro_admin.css">
 </head>
 <body>
@@ -13,15 +13,21 @@
     </header>
 
     <main>
-        <form action="neg_dat_registro_administrador.php" method="POST">
-            <label for="documento">Digitar su numero de documento</label>
-            <input type="number" id="documento" name="documento" placeholder="Digitar numero de documento" require>
-            <label for="nombre">Digitar sus nombres</label>
-            <input type="text" id="nombre" name="nombre" placeholder="Digitar sus nombres" require>
-            <label for="apellido">Digitar sus apellidos</label>
-            <input type="text" id="apellido" name="apellido" placeholder="Digitar su apellido" require>
-            <input type="submit" value="Enviar">
-        </form>
+        <section>
+            <h2>Crear cuenta de usuario</h2>
+            <form action="neg_dat_registro_administrador.php" method="POST">
+                <label for="documento">Digitar su numero de documento</label>
+                <input type="number" id="documento" name="documento" placeholder="Digitar numero de documento" require>
+                <label for="nombre">Digitar sus nombres</label>
+                <input type="text" id="nombre" name="nombre" placeholder="Digitar sus nombres" require>
+                <label for="apellido">Digitar sus apellidos</label>
+                <input type="text" id="apellido" name="apellido" placeholder="Digitar su apellido" require>
+                <div>
+                    <input type="submit" value="Enviar">
+                </div>
+                
+            </form>
+        </section>
     </main>
 </body>
 </html>
